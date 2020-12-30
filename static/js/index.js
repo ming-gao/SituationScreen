206d6a2738e9ea54e600f2060aacdf445a79652e
ming-gao <59911205+ming-gao@users.noreply.github.com>
2020-12-30 17:57:22 +0800

    字体添加

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,26 @@
 
 let lineCharts = echarts.init(document.getElementById('safe_line'))
-option = {
+lineoption = {
     xAxis: {
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        },
+        axisLabel:{
+            interVal:0,
+            rotate:-45
+        }
     },
     yAxis: {
-        type: 'value'
+        type: 'value',
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        }
     },
     series: [{
         data: [820, 932, 901, 934, 1290, 1330, 1320],
@@ -15,14 +29,69 @@ option = {
             color: '#1ABD95'
         }
 
-    },
-    {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'bar',
-        lineStyle: {
-            color: '#1ABD95'
-        }
     }
     ]
 }
-lineCharts.setOption(option)
+lineCharts.setOption(lineoption)
+
+let barCharts = echarts.init(document.getElementById('bar_normal'))
+baroption={
+    color: '#1ABD95',
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        },
+        axisLabel:{
+            interVal:0,
+            rotate:-45
+        }
+    },
+    yAxis: {
+        type: 'value',
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        }
+    },
+    series: [{
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+    }]
+}
+barCharts.setOption(baroption)
+
+let barExCharts = echarts.init(document.getElementById('bar_exchange'))
+baroption={
+    color: '#1ABD95',
+    xAxis: {
+        type: 'value',
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        },
+        axisLabel:{
+            interVal:0,
+            rotate:-45
+        }
+    },
+    yAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisLine:{
+            lineStyle:{
+                color:'#e2e2e2'
+            }
+        }
+    },
+    series: [{
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+    }]
+}
+barExCharts.setOption(baroption)
