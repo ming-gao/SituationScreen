4e73c3bd4d6ae002a18f27704b9304fbc8cb2cc4
ming-gao <59911205+ming-gao@users.noreply.github.com>
2020-12-31 10:06:30 +0800

    图表调整

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,11 @@
 
 let lineCharts = echarts.init(document.getElementById('safe_line'))
 lineoption = {
+    grid:{
+        left:'20%',
+        height:'70%',
+        top:'10%'
+    },
     xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
@@ -23,7 +28,7 @@ lineoption = {
         }
     },
     series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        data: [82, 93, 90, 93, 129, 133, 132],
         type: 'line',
         lineStyle: {
             color: '#1ABD95'
@@ -68,6 +73,10 @@ barCharts.setOption(baroption)
 let barExCharts = echarts.init(document.getElementById('bar_exchange'))
 baroption={
     color: '#1ABD95',
+    grid:{
+        height:'50%',
+        top:'10%'
+    },
     xAxis: {
         type: 'value',
         axisLine:{
