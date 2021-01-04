e74e9fd3fea7b60630b75c5d76f44e600ceffefc
CTL <474070613@qq.com>
2021-01-04 09:01:10 +0800

    布局调整

--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,29 @@
 
 let lineCharts = echarts.init(document.getElementById('safe_line'))
 lineoption = {
-    grid:{
-        left:'20%',
-        height:'70%',
-        top:'10%'
+    grid: {
+        left: '20%',
+        height: '70%',
+        top: '10%'
     },
     xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         },
-        axisLabel:{
-            interVal:0,
-            rotate:-45
+        axisLabel: {
+            interVal: 0,
+            rotate: -45
         }
     },
     yAxis: {
         type: 'value',
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         }
     },
@@ -40,26 +40,26 @@ lineoption = {
 lineCharts.setOption(lineoption)
 
 let barCharts = echarts.init(document.getElementById('bar_normal'))
-baroption={
+baroption = {
     color: '#1ABD95',
     xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         },
-        axisLabel:{
-            interVal:0,
-            rotate:-45
+        axisLabel: {
+            interVal: 0,
+            rotate: -45
         }
     },
     yAxis: {
         type: 'value',
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         }
     },
@@ -71,30 +71,30 @@ baroption={
 barCharts.setOption(baroption)
 
 let barExCharts = echarts.init(document.getElementById('bar_exchange'))
-baroption={
+baroption = {
     color: '#1ABD95',
-    grid:{
-        height:'50%',
-        top:'10%'
+    grid: {
+        height: '50%',
+        top: '10%'
     },
     xAxis: {
         type: 'value',
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         },
-        axisLabel:{
-            interVal:0,
-            rotate:-45
+        axisLabel: {
+            interVal: 0,
+            rotate: -45
         }
     },
     yAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisLine:{
-            lineStyle:{
-                color:'#e2e2e2'
+        axisLine: {
+            lineStyle: {
+                color: '#e2e2e2'
             }
         }
     },
@@ -106,4 +106,61 @@ baroption={
 barExCharts.setOption(baroption)
 
 
+let pieSetWebMon = echarts.init(document.getElementById("pie_set_webMon"));
+
+pieSetOption = {
+    color:["#99CDD3"],
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    // legend: {
+    //     orient: 'vertical',
+    //     left: 10,
+    //     data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+    // },
+    textStyle : {
+        fontSize : 2,
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            selectedMode: 'single',
+            radius: [0, '30%'],
+
+            label: {
+                position: 'inner'
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                { value: 335, name: '直达', selected: true },
+                { value: 679, name: '营销广告' },
+                { value: 1548, name: '搜索引擎' }
+            ]
+        },
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['40%', '55%'],
+            label: {
+               position : 'inner',
+            },
+            data: [
+                { value: 335, name: '直达' },
+                { value: 310, name: '邮件营销' },
+                { value: 234, name: '联盟广告' },
+                { value: 135, name: '视频广告' },
+                { value: 1048, name: '百度' },
+                { value: 251, name: '谷歌' },
+                { value: 147, name: '必应' },
+                { value: 102, name: '其他' }
+            ]
+        }
+    ]
+};
+
+pieSetWebMon.setOption(pieSetOption)
 
